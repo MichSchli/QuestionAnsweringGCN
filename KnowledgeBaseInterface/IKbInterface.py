f9e02bbc056f3b6dbaf19f654adad0f67ba459e7
MichSchli <michael.sejr@gmail.com>
2017-07-31 17:22:21 +0100

    jo

--- KnowledgeBaseInterface/IKbInterface.py
+++ KnowledgeBaseInterface/IKbInterface.py
@@ -7,7 +7,7 @@ class IKbInterface:
             hops -1
             remaining_edges = edge_limit-len(seen_edges)
 
-            frontier, edges = self.get_one_neighborhood(frontier, limit=remaining_edges)
+            frontier, edges = self.retrieve_one_neighborhood(frontier, limit=remaining_edges)
             seen_edges.append(edges)
 
         return seen_edges
