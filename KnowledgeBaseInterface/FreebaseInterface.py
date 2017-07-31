a8cc0f480f231ae30959120fd6be47b1732473e1
MichSchli <michael.sejr@gmail.com>
2017-07-31 16:56:06 +0100

    ada

--- KnowledgeBaseInterface/FreebaseInterface.py
+++ KnowledgeBaseInterface/FreebaseInterface.py
@@ -1,5 +1,5 @@
 from SPARQLWrapper import SPARQLWrapper, JSON
-
+import numpy
 from KnowledgeBaseInterface.IKbInterface import IKbInterface
 
 
@@ -44,9 +44,9 @@ class FreebaseInterface(IKbInterface):
 
         tuple_results = [None]* len(results["results"]["bindings"])
         for i,result in enumerate(results["results"]["bindings"]):
-            tuple_results = (result["s"]["value"], result["r"]["value"], result["o"]["value"])
+            tuple_results = [result["s"]["value"], result["r"]["value"], result["o"]["value"]]
 
-        return tuple_results
+        return np.array(tuple_results)
 
 if __name__ == "__main__":
     iface = FreebaseInterface()
