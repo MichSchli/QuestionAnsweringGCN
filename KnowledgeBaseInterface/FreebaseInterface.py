6f84276384a28a6fcbba85598df4ceec9270961a
MichSchli <michael.sejr@gmail.com>
2017-07-31 16:25:20 +0100

    Small update

--- KnowledgeBaseInterface/FreebaseInterface.py
+++ KnowledgeBaseInterface/FreebaseInterface.py
@@ -47,3 +47,9 @@ class FreebaseInterface(IKbInterface):
             tuple_results = (result["s"]["value"], result["r"]["value"], result["o"]["value"])
 
         return tuple_results
+
+if __name__ == "__main__":
+    iface = FreebaseInterface()
+    results = iface.retrieve_one_neighborhood_graph(["ns:m.014zcr", "ns:m.0q0b4"], limit=3000)
+    for result in results:
+        print(result)
