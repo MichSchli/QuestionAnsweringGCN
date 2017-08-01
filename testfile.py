b73039b1fd7542ca4677973015916def03f5e68a
MichSchli <michael.sejr@gmail.com>
2017-08-01 15:28:27 +0100

    Changed testfile

--- testfile.py
+++ testfile.py
@@ -1,6 +1,17 @@
-from KnowledgeBaseInterface.FreebaseInterface import FreebaseInterface
+from grounding.json_to_candidate_neighborhood import parse_from_file
+import argparse
 
-iface = FreebaseInterface()
-results = iface.get_neighborhood(["m.014zcr", "m.0q0b4"], edge_limit=4000, hops=2)
-print(results[0].shape)
-print(results[1].shape)
+#from KnowledgeBaseInterface.FreebaseInterface import FreebaseInterface
+
+#iface = FreebaseInterface()
+#results = iface.get_neighborhood(["m.014zcr", "m.0q0b4"], edge_limit=4000, hops=2)
+#print(results[0].shape)
+#print(results[1].shape)
+
+parser = argparse.ArgumentParser(description='Yields candidate graphs to stdout.')
+parser.add_argument('--file', type=str, help='The location of the .json-file to be parsed')
+
+args = parser.parse_args()
+
+if args.file is not None:
+    parse_from_file(args.file)
