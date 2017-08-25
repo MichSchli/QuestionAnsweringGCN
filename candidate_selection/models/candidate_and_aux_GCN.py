5f38518e6944e776e8dc032eccca4bd190fa8e87
Michael Schlichtkrull <mschlic1@logicgpu2.science.uva.nl>
2017-08-25 13:24:58 +0200

    Reduce dim

--- candidate_selection/models/candidate_and_aux_GCN.py
+++ candidate_selection/models/candidate_and_aux_GCN.py
@@ -31,7 +31,7 @@ class CandidateAndAuxGcnModel:
 
     aux_iterator = None
 
-    def __init__(self, facts, aux_iterator, layers=1, dimension=18):
+    def __init__(self, facts, aux_iterator, layers=1, dimension=6):
         self.dimension = dimension
         self.entity_dict = {}
         self.facts = facts
