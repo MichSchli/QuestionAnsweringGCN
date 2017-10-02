bc7e6fd78534bb4fbc5f6e6422caf6fd692c2641
MichSchli <michael.sejr@gmail.com>
2017-10-02 10:32:14 +0100

    2 edges

--- track_edges_to_gold.py
+++ track_edges_to_gold.py
@@ -25,8 +25,8 @@ def generate_1_query(centroids, golds, forward_edges=True):
 
     return query
 
-def get_1_edges(centroids, golds):
-    query = generate_1_query(sentence, gold)
+def get_1_paths(centroids, golds):
+    query = generate_1_query(centroids, golds)
     sparql.setQuery(query)
     sparql.setReturnFormat(JSON)
     results = sparql.query().convert()
@@ -34,7 +34,7 @@ def get_1_edges(centroids, golds):
     for r in results["results"]["bindings"]:
         yield r["r"]
 
-    query = generate_1_query(sentence, gold, forward_edges=False)
+    query = generate_1_query(centroids, golds, forward_edges=False)
     sparql.setQuery(query)
     sparql.setReturnFormat(JSON)
     results = sparql.query().convert()
@@ -42,21 +42,39 @@ def get_1_edges(centroids, golds):
     for r in results["results"]["bindings"]:
         yield r["r"]
 
-def generate_2_query(centroids, golds, forward_edges=True):
-    centroid_symbol = "s" if forward_edges else "o"
-    gold_symbol = "o" if forward_edges else "s"
+def generate_2_query(centroids, golds, forward_1_edges=True, forward_2_edges=True):
+    centroid_symbol = "s"
+    gold_symbol = "o"
+
+    first_edge_string = "?s ?r1 ?i ." if forward_1_edges else "?i ?r1 ?s"
+    second_edge_string = "?i ?r2 ?o ." if forward_2_edges else "?o ?r2 ?i"
 
     query = "PREFIX ns: <http://rdf.freebase.com/ns/>"
     query += "\n\nselect * where {"
-    query += "\n\t?s ?r ?i ."
-    query += "\n\t?i ?r ?o ."
+    query += "\n\t" + first_edge_string + " ."
+    query += "\n\t" + second_edge_string + " ."
     query += "\n\tvalues ?" + centroid_symbol + " { " + " ".join(centroids) + " }"
     query += "\n\tvalues ?" + gold_symbol + " { " + " ".join(["\""+g+"\"" for g in golds]) + " }"
     query += "\n}"
 
     return query
 
+def get_2_paths(centroids, golds):
+    yield from get_2_paths_internal(centroids, golds, True, True)
+    yield from get_2_paths_internal(centroids, golds, True, False)
+    yield from get_2_paths_internal(centroids, golds, False, True)
+    yield from get_2_paths_internal(centroids, golds, False, False)
+
+
+def get_2_paths_internal(centroids, golds, forward_1, forward_2):
+    query = generate_2_query(centroids, golds, forward_1, forward_2)
+    sparql.setQuery(query)
+    sparql.setReturnFormat(JSON)
+    results = sparql.query().convert()
+    for r in results["results"]["bindings"]:
+        yield r["r1"], r["r2"]
+
 
 for gold, sentence in zip(gold_reader.parse_file(args.file), sentence_reader.parse_file(args.file)):
-    for edge in get_1_edges(sentence, gold):
+    for edge in get_1_paths(sentence, gold):
         print(edge)
