a9a7dba4f54ecdc96baae402c30ccedefa424242
MichSchli <michael.sejr@gmail.com>
2017-10-02 10:03:57 +0100

    initial query

--- track_edges_to_gold.py
+++ track_edges_to_gold.py
@@ -1,23 +1,27 @@
-#sparql.setQuery("""
-#PREFIX ns: <http://rdf.freebase.com/ns/>
-
-#select count(*) where {
-#?s ?r ?o .
-#values ?s { ns:m.03_r3 }
-#}
-#"""
+from SPARQLWrapper import SPARQLWrapper, JSON
 import argparse
-
 from preprocessing.read_conll_files import ConllReader
 
 parser = argparse.ArgumentParser(description='Yields pairs of prediction from a strategy and gold to stdout.')
 parser.add_argument('--file', type=str, help='The location of the .conll-file to be parsed')
 args = parser.parse_args()
 
+sparql = SPARQLWrapper("http://localhost:8890/sparql")
+
 gold_reader = ConllReader(output="gold")
 sentence_reader = ConllReader(output="entities", entity_prefix="ns:")
 
-for gold, sentence in zip(gold_reader.parse_file(args.file), sentence_reader.parse_file(args.file)):
-    print(gold)
-    print(sentence)
-    exit()
+def generate_1_query(centroids, golds):
+    query = "PREFIX ns: <http://rdf.freebase.com/ns/>"
+    query += "\n\nselect count(*) where {"
+    query += "\n?s ?r ?o ."
+    query += "\nvalues ?s { " + " ".join(centroids) + " }"
+
+
+for gold, sentence in zip(gold_reader.parse_file(args.file)[:3], sentence_reader.parse_file(args.file)[:3]):
+    query = generate_1_query(sentence, gold)
+    sparql.setQuery(query)
+    sparql.setReturnFormat(JSON)
+    results = sparql.query().convert()
+
+    print(results)
