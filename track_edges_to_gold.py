63f44b320b8e9d92b47805c29734a3775dec73cb
MichSchli <michael.sejr@gmail.com>
2017-10-02 09:55:56 +0100

    initial edge tracker

---
+++ track_edges_to_gold.py
@@ -0,0 +1,21 @@
+#sparql.setQuery("""
+#PREFIX ns: <http://rdf.freebase.com/ns/>
+
+#select count(*) where {
+#?s ?r ?o .
+#values ?s { ns:m.03_r3 }
+#}
+#"""
+import argparse
+
+from preprocessing.read_conll_files import ConllReader
+
+parser = argparse.ArgumentParser(description='Yields pairs of prediction from a strategy and gold to stdout.')
+parser.add_argument('--file', type=str, help='The location of the .conll-file to be parsed')
+args = parser.parse_args()
+
+gold_reader = ConllReader(output="gold")
+sentence_reader = ConllReader()
+
+for gold, sentence in zip(gold_reader.parse_file(args.file), sentence_reader.parse_file(args.file)):
+    pass
