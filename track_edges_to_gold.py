5d8d09db3ce0a4f4c6327bd8402eab370cb4706d
MichSchli <michael.sejr@gmail.com>
2017-10-02 10:16:32 +0100

    test 1 edge

--- track_edges_to_gold.py
+++ track_edges_to_gold.py
@@ -1,6 +1,7 @@
 from SPARQLWrapper import SPARQLWrapper, JSON
 import argparse
 from preprocessing.read_conll_files import ConllReader
+import itertools
 
 parser = argparse.ArgumentParser(description='Yields pairs of prediction from a strategy and gold to stdout.')
 parser.add_argument('--file', type=str, help='The location of the .conll-file to be parsed')
@@ -15,11 +16,12 @@ def generate_1_query(centroids, golds, forward_edges=True):
     centroid_symbol = "s" if forward_edges else "o"
     gold_symbol = "o" if forward_edges else "s"
 
+    combinations = ["( "+" ".join(p)+" )" for p in itertools.product(centroids, golds)]
+
     query = "PREFIX ns: <http://rdf.freebase.com/ns/>"
     query += "\n\nselect * where {"
     query += "\n\t?s ?r ?o ."
-    query += "\n\tvalues ?" + centroid_symbol + " { " + " ".join(centroids) + " }"
-    query += "\n\tvalues ?" + gold_symbol + " { " + " ".join(golds) + " }"
+    query += "\n\tvalues ?" + centroid_symbol + " ?" + gold_symbol + " { " + " ".join(combinations) + " }"
     query += "\n}"
 
     return query
