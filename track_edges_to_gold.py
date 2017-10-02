07f43eedc2fb7b1a02c788822fb42158d3733aac
MichSchli <michael.sejr@gmail.com>
2017-10-02 10:22:44 +0100

    test

--- track_edges_to_gold.py
+++ track_edges_to_gold.py
@@ -17,8 +17,39 @@ def generate_1_query(centroids, golds, forward_edges=True):
     gold_symbol = "o" if forward_edges else "s"
 
     query = "PREFIX ns: <http://rdf.freebase.com/ns/>"
-    query += "\n\nselect * where {"
+    query += "\n\nselect r where {"
     query += "\n\t?s ?r ?o ."
+    query += "\n\tvalues ?" + centroid_symbol + " { " + " ".join(centroids) + " }"
+    query += "\n\tvalues ?" + gold_symbol + " { " + " ".join(["\""+g+"\"" for g in golds]) + " }"
+    query += "\n}"
+
+    return query
+
+def get_1_edges(centroids, golds):
+    query = generate_1_query(sentence, gold)
+    sparql.setQuery(query)
+    sparql.setReturnFormat(JSON)
+    results = sparql.query().convert()
+
+    for r in results["results"]["bindings"]:
+        yield r["r"]
+
+    query = generate_1_query(sentence, gold, forward_edges=False)
+    sparql.setQuery(query)
+    sparql.setReturnFormat(JSON)
+    results = sparql.query().convert()
+
+    for r in results["results"]["bindings"]:
+        yield r["r"]
+
+def generate_2_query(centroids, golds, forward_edges=True):
+    centroid_symbol = "s" if forward_edges else "o"
+    gold_symbol = "o" if forward_edges else "s"
+
+    query = "PREFIX ns: <http://rdf.freebase.com/ns/>"
+    query += "\n\nselect * where {"
+    query += "\n\t?s ?r ?i ."
+    query += "\n\t?i ?r ?o ."
     query += "\n\tvalues ?" + centroid_symbol + " { " + " ".join(centroids) + " }"
     query += "\n\tvalues ?" + gold_symbol + " { " + " ".join(["\""+g+"\"" for g in golds]) + " }"
     query += "\n}"
@@ -27,11 +58,5 @@ def generate_1_query(centroids, golds, forward_edges=True):
 
 
 for gold, sentence in zip(gold_reader.parse_file(args.file), sentence_reader.parse_file(args.file)):
-    query = generate_1_query(sentence, gold)
-    print(query)
-    sparql.setQuery(query)
-    sparql.setReturnFormat(JSON)
-    results = sparql.query().convert()
-
-    print(results)
-    exit()
+    for edge in get_1_edges(sentence, gold):
+        print(edge)
