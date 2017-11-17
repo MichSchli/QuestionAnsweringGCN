f1bdd6fc8bf535df284f13d5491ef5c4a788fdac
MichSchli <michael.sejr@gmail.com>
2017-11-17 11:54:15 +0000

    try stuff

--- model/hypergraph_model.py
+++ model/hypergraph_model.py
@@ -224,6 +224,7 @@ class HypergraphModel:
             frontier = all_v[np.isin(all_v, visited_v, assume_unique=True, invert=True)]
             visited_v = np.concatenate((visited_v, frontier))
 
+        print(self.entity_vertices.shape)
         print(visited_v)
         print(visited_v.shape)
         self.entity_vertices = self.entity_vertices[visited_v]
