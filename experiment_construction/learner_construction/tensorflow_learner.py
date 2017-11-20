ece4348eacef7f79ba3ae14b11a73d742d113828
MichSchli <michael.sejr@gmail.com>
2017-11-20 18:41:04 +0000

    fixed bug

--- experiment_construction/learner_construction/tensorflow_learner.py
+++ experiment_construction/learner_construction/tensorflow_learner.py
@@ -95,8 +95,10 @@ class TensorflowModel:
                     gold_list.extend(l)
                 else:
                     gold_list.append(name)
+
+            gold_list = np.array(gold_list).astype(np.int32)
             print(example["neighborhood"].entity_vertices.shape[0])
-            print("Swapping " + str(example["gold_entities"]) + " for " + str(gold_list.astype(np.int32)))
+            print("Swapping " + str(example["gold_entities"]) + " for " + str(gold_list))
 
             example["gold_entities"] = gold_list
             yield example
