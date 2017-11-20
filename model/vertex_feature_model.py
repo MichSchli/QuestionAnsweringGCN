01f0493a151e2d2441aacfb1c995169fc7bb7392
MichSchli <michael.sejr@gmail.com>
2017-11-20 18:26:11 +0000

    fixed bug

--- model/vertex_feature_model.py
+++ model/vertex_feature_model.py
@@ -18,8 +18,8 @@ class VertexFeatureModel:
     def inverse_project(self, features):
         vertices = {feature: [] for feature in features}
         for edge in self.feature_projection:
-            if self.feature_map[edge[1]] in features:
-                vertices[self.feature_map[edge[1]]].append(edge[0])
+            if self.features[edge[1]] in features:
+                vertices[self.features[edge[1]]].append(edge[0])
 
         vertices = {n[0]: np.unique(n[1]) for n in vertices.items()}
         print(vertices)
