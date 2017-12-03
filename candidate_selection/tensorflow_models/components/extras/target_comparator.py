29d75f1f9f4393e207a4bb8ff2d4a15578fd2760
MichSchli <michael.sejr@gmail.com>
2017-12-03 15:44:54 +0000

    removed print

--- candidate_selection/tensorflow_models/components/extras/target_comparator.py
+++ candidate_selection/tensorflow_models/components/extras/target_comparator.py
@@ -20,7 +20,7 @@ class TargetComparator(AbstractComponent):
     def get_comparison_scores(self, all_target_embeddings, element_embeddings):
         all_target_embeddings = tf.Print(all_target_embeddings, [all_target_embeddings], summarize=25, message="Target embeddings: ")
         target_embeddings = tf.nn.embedding_lookup(all_target_embeddings, self.variables.get_variable(self.variable_prefix + "target_indices"))
-        target_embeddings = tf.Print(target_embeddings, [self.variables.get_variable(self.variable_prefix + "target_indices")], summarize=25)
+        #target_embeddings = tf.Print(target_embeddings, [self.variables.get_variable(self.variable_prefix + "target_indices")], summarize=25)
 
         if self.comparison == "dot_product":
             comparison = tf.reduce_sum(target_embeddings * element_embeddings, axis=1)
