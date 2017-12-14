5742ef51d533f2ed3fa75cd58518a66506537546
MichSchli <michael.sejr@gmail.com>
2017-12-14 17:49:28 +0000

    test print

--- experiment_construction/learner_construction/tensorflow_learner.py
+++ experiment_construction/learner_construction/tensorflow_learner.py
@@ -211,7 +211,7 @@ class TensorflowModel:
 
                 for index,prob in l[:5]:
                     if prob > 0.5:
-                        print(batch["neighborhood"].get_paths_to_neighboring_centroid(index))
+                        print(batch["neighborhood"][i].get_paths_to_neighboring_centroid(index))
 
                 yield [(batch["neighborhood"][i].from_index_with_names(index),prob) for index,prob in l]
                 continue
