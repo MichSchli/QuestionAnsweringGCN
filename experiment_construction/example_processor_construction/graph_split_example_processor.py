92793449a7e866225bb32173d7b69579dbe69d0c
MichSchli <michael.sejr@gmail.com>
2018-01-08 12:29:44 +0000

    names

--- experiment_construction/example_processor_construction/graph_split_example_processor.py
+++ experiment_construction/example_processor_construction/graph_split_example_processor.py
@@ -7,7 +7,8 @@ class GraphSplitExampleProcessor(AbstractExampleProcessor):
     def process_example(self, example, mode="train"):
         graph = example["neighborhood"]
         centroids = [example["neighborhood"].to_index(c) for c in example["sentence_entity_map"][:, 2]]
-        centroids = np.concatenate(centroids)
+        if centroids.shape[0] > 0:
+            centroids = np.concatenate(centroids)
         example["neighborhood"].set_centroids(centroids)
         example["neighborhood"] = graph.get_split_graph()
 
