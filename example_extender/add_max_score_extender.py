29a5c1eacffe44b3cdcd5589baa3a61a8f4ae255
MichSchli <michael.sejr@gmail.com>
2018-04-27 11:28:58 +0100

    fixed bug

--- example_extender/add_max_score_extender.py
+++ example_extender/add_max_score_extender.py
@@ -11,11 +11,11 @@ class AddMaxScoreExtender:
     def extend(self, example):
         example = self.inner.extend(example)
 
+        if not example.has_mentions():
+            return example
+
         for i in range(example.graph.vertices.shape[0]):
             max_score = max([example.mentions[j].score for j in example.graph.nearby_centroid_map[i]])
             example.graph.vertex_max_scores[i] = max_score
-
-        if not example.has_mentions():
-            return example
 
         return example
