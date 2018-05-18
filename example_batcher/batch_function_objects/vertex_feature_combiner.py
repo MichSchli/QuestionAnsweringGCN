996789dacb7fa32167fa614e1d5857a01673010f
MichSchli <michael.sejr@gmail.com>
2018-05-18 12:44:40 +0100

    fix

--- example_batcher/batch_function_objects/vertex_feature_combiner.py
+++ example_batcher/batch_function_objects/vertex_feature_combiner.py
@@ -9,11 +9,11 @@ class VertexFeatureCombiner:
         self.batch = batch
 
     def get_max_score_by_vertex(self):
-        index_lists = [example.get_vertex_max_scores() for example in self.examples]
+        index_lists = [example.get_vertex_max_scores() for example in self.batch.examples]
 
         return np.concatenate(index_lists).astype(np.float32)
 
     def get_combined_vertex_types(self):
-        lists = [example.graph.get_vertex_types() for example in self.examples]
+        lists = [example.graph.get_vertex_types() for example in self.batch.examples]
 
         return np.concatenate(lists)
