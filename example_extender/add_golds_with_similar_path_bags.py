a60702328943a488213c0d34f64feb164a05cdd9
MichSchli <michael.sejr@gmail.com>
2018-05-18 12:42:35 +0100

    fix

--- example_extender/add_golds_with_similar_path_bags.py
+++ example_extender/add_golds_with_similar_path_bags.py
@@ -20,36 +20,42 @@ class AddGoldsWithSimilarPathBags:
 
         additional_gold_answers = []
 
-        for i,gold_answer in enumerate(example.gold_answers):
-            for index in gold_answer.entity_indexes:
-                gold_path_bag = example.graph.entity_centroid_paths[index]
-                for j in example.graph.entity_vertex_indexes:
-                    if j in example.get_gold_indexes():
-                        continue
+        for j in example.graph.entity_vertex_indexes:
+            if j in example.get_gold_indexes():
+                continue
 
-                    j_path_bag = example.graph.entity_centroid_paths[j]
+            add = False
 
+            for i, gold_answer in enumerate(example.gold_answers):
+                for index in gold_answer.entity_indexes:
+                    gold_path_bag = example.graph.entity_centroid_paths[index]
+                    j_path_bag = np.array(example.graph.entity_centroid_paths[j])
                     overlap = np.in1d(gold_path_bag, j_path_bag)
-
                     overlap_rate = np.sum(overlap) / len(gold_path_bag)
 
                     if overlap_rate > self.similarity:
-                        gold_answer = GoldAnswer()
-                        if self.project_names:
-                            gold_answer.entity_name_or_label = example.graph.map_to_name_or_label(j)
-                            gold_answer.entity_indexes = np.array(
-                                example.graph.map_from_name_or_label(gold_answer.entity_name_or_label))
-                        else:
-                            gold_answer.entity_name_or_label = example.graph.map_to_label(j)
-                            gold_answer.entity_indexes = np.array(
-                                [example.graph.map_from_label(gold_answer.entity_name_or_label)])
-                        additional_gold_answers.append(gold_answer)
+                        print(gold_path_bag[overlap])
+                        add = True
 
-                        gold_answer.entity_indexes = gold_answer.entity_indexes[
-                            np.where(gold_answer.entity_indexes >= 0)]
+            if add:
+                gold_answer = GoldAnswer()
+                if self.project_names:
+                    gold_answer.entity_name_or_label = example.graph.map_to_name_or_label(j)
+                    gold_answer.entity_indexes = np.array(
+                        example.graph.map_from_name_or_label(gold_answer.entity_name_or_label))
+                else:
+                    gold_answer.entity_name_or_label = example.graph.map_to_label(j)
+                    gold_answer.entity_indexes = np.array(
+                        [example.graph.map_from_label(gold_answer.entity_name_or_label)])
+                additional_gold_answers.append(gold_answer)
 
-        example.gold_answers.extend(additional_gold_answers)
-        #print(example)
-        #print("====================================")
+                gold_answer.entity_indexes = gold_answer.entity_indexes[
+                    np.where(gold_answer.entity_indexes >= 0)]
+
+        if len(additional_gold_answers) > 0:
+            print(example)
+            example.gold_answers.extend(additional_gold_answers)
+            print(example)
+            print("====================================")
 
         return example
